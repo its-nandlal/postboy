b5fe3643e5274eae08429a3daeb4fa82eb2708b7
its-nandlal <nandlaljangir41@gmail.com>
2025-10-15 16:22:39 +0530

    02_database

---
+++ src/lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+    var prisma: PrismaClient | undefined
+}
+
+const db = global.prisma || new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+})
+
+
+if (process.env.NODE_ENV !== 'development') globalThis.prisma = db
+
+export default db
